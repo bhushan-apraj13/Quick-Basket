a53c6eb00eca3ee058951ef987bf646a5c9080c0
Bhushan Apraj <bhushanapraj69@gmail.com>
2025-02-07 18:34:50 +0530

    env setup done completely authentication started, working on login page -43:00

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './auth/login'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <button>lets build it</button>
+      <Login />
     </>
   )
 }
---
+++ client/src/auth/login.tsx
@@ -0,0 +1,33 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Mail } from "lucide-react";
+
+const Login = () => {
+    return (
+
+        <div className="flex items-center justify-center min-h-screen">
+
+            <form className="md:p-8 w-full max-w-md  rounded-lg md:border border-gray-200 mx-4">
+                <div className="mb-4">
+                    <h1 className="font-bold text-2xl">QuickBasket</h1>
+                </div>
+                <div className="relative">
+                    <Input
+                        type="email"
+                        placeholder="Enter your email" className="pl-10 border border-gray-300 focus:border-gray-300 focus:ring-0 focus:ring-gray-300"
+                    />
+                    <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+                </div>
+
+                <Label>Password</Label>
+                <Input
+                    type="password"
+                    placeholder="Enter your password"
+
+                />
+            </form>
+        </div>
+    );
+};
+
+export default Login;
