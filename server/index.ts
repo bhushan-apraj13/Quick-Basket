d68ef9645852c864f180110962952a17084fd7a3
Bhushan Apraj <bhushanapraj69@gmail.com>
2025-03-13 20:20:20 +0530

    Ready for deploying

--- server/index.ts
+++ server/index.ts
@@ -10,9 +10,12 @@ import userRoute from "./routes/user.route";
 import shopRoute from "./routes/shop.route";
 import productRoute from "./routes/product.route";
 import orderRoute from "./routes/order.route";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const DIRNAME = path.resolve();
 
 // default middlewares 
 app.use(bodyParser.json({ limit: '10mb' }));
@@ -35,6 +38,10 @@ app.use("/api/v1/shop", shopRoute);
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/order", orderRoute);
 
+app.use(express.static(path.join(DIRNAME,"/client/dist")));
+app.use("*",(_,res) =>{
+    res.sendFile(path.resolve(DIRNAME, "client", "dist", "index.html"));
+});
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`);
